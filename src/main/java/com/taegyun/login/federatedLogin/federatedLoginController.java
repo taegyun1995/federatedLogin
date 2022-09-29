8c03dede31bc876a1fcce22b0e02fd79fe49ca02
이태균 <>
2022-09-29 09:48:49 +0900

    a

--- src/main/java/com/taegyun/login/federatedLogin/federatedLoginController.java
+++ src/main/java/com/taegyun/login/federatedLogin/federatedLoginController.java
@@ -2,7 +2,6 @@ package com.taegyun.login.federatedLogin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class federatedLoginController {
